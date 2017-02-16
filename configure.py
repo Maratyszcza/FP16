7f5fc4a73f7ebc4b0b9843ce6d64b6a6eb5285d4
Marat Dukhan <maratek@gmail.com>
2017-02-16 17:17:49 -0500

    Unit test for bitcasts

--- configure.py
+++ configure.py
@@ -13,19 +13,22 @@ def main(args):
 
     with build.options(source_dir="test", extra_include_dirs="test", deps=build.deps.googletest):
         fp16_tables = build.cxx("tables.cc")
-        build.unittest("ieee-from-fp32-value",
-            [build.cxx("ieee-from-fp32-value.cc"), fp16_tables])
         build.unittest("ieee-to-fp32-bits",
             [build.cxx("ieee-to-fp32-bits.cc"), fp16_tables])
         build.unittest("ieee-to-fp32-value",
             [build.cxx("ieee-to-fp32-value.cc"), fp16_tables])
 
-        build.unittest("alt-from-fp32-value",
-            [build.cxx("alt-from-fp32-value.cc"), fp16_tables])
         build.unittest("alt-to-fp32-bits",
             [build.cxx("alt-to-fp32-bits.cc"), fp16_tables])
         build.unittest("alt-to-fp32-value",
             [build.cxx("alt-to-fp32-value.cc"), fp16_tables])
+
+        build.unittest("ieee-from-fp32-value",
+            [build.cxx("ieee-from-fp32-value.cc"), fp16_tables])
+        build.unittest("alt-from-fp32-value",
+            [build.cxx("alt-from-fp32-value.cc"), fp16_tables])
+
+        build.unittest("bitcasts", build.cxx("bitcasts.cc"))
 
     with build.options(source_dir="bench", deps=build.deps.googlebenchmark):
         build.benchmark("ieee-bench", build.cxx("ieee.cc"))
