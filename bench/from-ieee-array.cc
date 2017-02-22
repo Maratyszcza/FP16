7840009ca0ed728d23ef0f1fadf7bda86a5b97f4
Marat Dukhan <maratek@gmail.com>
2017-02-22 13:17:35 -0500

    Benchmark 256-bit F16C FP16->FP32 conversion

--- bench/from-ieee-array.cc
+++ bench/from-ieee-array.cc
@@ -136,7 +136,7 @@ BENCHMARK(fp16_ieee_to_fp32_value)->RangeMultiplier(2)->Range(1<<10, 64<<20);
 #endif
 
 #if (defined(__i386__) || defined(__x86_64__)) && defined(__F16C__)
-	static void fp16_ieee_to_fp32_hardware(benchmark::State& state) {
+	static void hardware_mm_cvtph_ps(benchmark::State& state) {
 		const uint_fast32_t seed = std::chrono::system_clock::now().time_since_epoch().count();
 		auto rng = std::bind(std::uniform_real_distribution<float>(-1.0f, 1.0f), std::mt19937(seed));
 
@@ -161,7 +161,34 @@ BENCHMARK(fp16_ieee_to_fp32_value)->RangeMultiplier(2)->Range(1<<10, 64<<20);
 		}
 		state.SetItemsProcessed(int64_t(state.iterations()) * int64_t(state.range(0)));
 	}
-	BENCHMARK(fp16_ieee_to_fp32_hardware)->RangeMultiplier(2)->Range(1<<10, 64<<20);
+	BENCHMARK(hardware_mm_cvtph_ps)->RangeMultiplier(2)->Range(1<<10, 64<<20);
+
+	static void hardware_mm256_cvtph_ps(benchmark::State& state) {
+		const uint_fast32_t seed = std::chrono::system_clock::now().time_since_epoch().count();
+		auto rng = std::bind(std::uniform_real_distribution<float>(-1.0f, 1.0f), std::mt19937(seed));
+
+		std::vector<uint16_t> fp16(state.range(0));
+		std::vector<float> fp32(state.range(0));
+		std::generate(fp16.begin(), fp16.end(),
+			[&rng]{ return fp16_ieee_from_fp32_value(rng()); });
+
+		while (state.KeepRunning()) {
+			uint16_t* input = fp16.data();
+			benchmark::DoNotOptimize(input);
+
+			float* output = fp32.data();
+			const size_t n = state.range(0);
+			for (size_t i = 0; i < n; i += 8) {
+				_mm256_storeu_ps(&output[i],
+					_mm256_cvtph_ps(
+						_mm_loadu_si128(static_cast<const __m128i*>(static_cast<const void*>(&input[i])))));
+			}
+
+			benchmark::DoNotOptimize(output);
+		}
+		state.SetItemsProcessed(int64_t(state.iterations()) * int64_t(state.range(0)));
+	}
+	BENCHMARK(hardware_mm256_cvtph_ps)->RangeMultiplier(2)->Range(1<<10, 64<<20);
 #endif
 
 #ifdef FP16_COMPARATIVE_BENCHMARKS
