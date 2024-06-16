c2c86b88b1f5e803ab4dc1a7302c8d271d0ff875
Marat Dukhan <maratek@gmail.com>
2024-06-16 13:13:58 -0700

    Use hardware-accelerated _Float16 type with x86 GCC

--- include/fp16/macros.h
+++ include/fp16/macros.h
@@ -3,6 +3,17 @@
 #define FP16_MACROS_H
 
 
+#ifndef FP16_USE_FLOAT16_TYPE
+	#if !defined(__clang__) && !defined(__INTEL_COMPILER) && defined(__GNUC__) && (__GNUC__ >= 12)
+		#if defined(__F16C__)
+			#define FP16_USE_FLOAT16_TYPE 1
+		#endif
+	#endif
+	#if !defined(FP16_USE_FLOAT16_TYPE)
+		#define FP16_USE_FLOAT16_TYPE 0
+	#endif  // !defined(FP16_USE_FLOAT16_TYPE)
+#endif  // !defined(FP16_USE_FLOAT16_TYPE)
+
 #ifndef FP16_USE_FP16_TYPE
 	#if defined(__clang__)
 		#if defined(__F16C__) || defined(__aarch64__)
