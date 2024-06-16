bc497e28844ff327023e95273938b8c128f98886
Marat Dukhan <maratek@gmail.com>
2024-06-16 13:01:45 -0700

    Use hardware-accelerated __fp16 type with AArch64 GCC

--- include/fp16/macros.h
+++ include/fp16/macros.h
@@ -8,6 +8,10 @@
 		#if defined(__F16C__) || defined(__aarch64__)
 			#define FP16_USE_FP16_TYPE 1
 		#endif
+	#elif defined(__GNUC__)
+		#if defined(__aarch64__)
+			#define FP16_USE_FP16_TYPE 1
+		#endif
 	#endif
 	#if !defined(FP16_USE_FP16_TYPE)
 		#define FP16_USE_FP16_TYPE 0
